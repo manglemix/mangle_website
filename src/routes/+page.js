339ecb5c47a8167bbd161fbb510bacec2c1c911c
Najman <najman.husaini@utah.edu>
2022-11-21 21:42:09 -0700

    correct api call

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,9 +4,7 @@ import { page } from '$app/stores';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, url }) {
-    let splitted = url.origin.split(":");
-    
-    let response = await fetch(splitted[0] + ":" + splitted[1] + ':10000/blogs?count=5');
+    let response = await fetch(url.origin + 'api/blogs?count=5');
 
     if (!response.ok) {
         throw error(500, "Could not load blogs");
