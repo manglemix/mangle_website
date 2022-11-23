ee3fca9a1c928a38593b0800a03e770a50a89d88
Najman <najman.husaini@utah.edu>
2022-11-22 21:33:38 -0700

    added cors fix

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,9 @@
 import { error } from '@sveltejs/kit';
-import { page } from '$app/stores';
 
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, url }) {
-    let response = await fetch(url.origin + 'api/blogs?count=5');
+    let response = await fetch('https://manglemix.com/api/blogs?count=5');
 
     if (!response.ok) {
         throw error(500, "Could not load blogs");
