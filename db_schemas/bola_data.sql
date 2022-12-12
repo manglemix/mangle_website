fa090248a430a8b32f288f5d2ee3da57876d6558
Najman <najman.husaini@utah.edu>
2022-12-12 16:19:42 -0700

    Updated sql table and added more commands

--- db_schemas/bola_data.sql
+++ db_schemas/bola_data.sql
@@ -2,6 +2,7 @@ CREATE TABLE EndlessLeaderboard (
     Username NVARCHAR(16) NOT NULL,
     Difficulty TINYINT NOT NULL,
     Levels SMALLINT NOT NULL,
+    Time DOUBLE NOT NULL,
     unique (Username, Difficulty)
 );
 CREATE TABLE TournamentWinners (
