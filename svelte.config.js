866628fdc2dc82bcaebbe7f05d885e356d429c9b
Najman <najman.husaini@utah.edu>
2022-12-25 21:44:09 +0800

    revert to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,15 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
-export default {
-  kit: {
-    // default options are shown
-    adapter: vercel({
-      // if true, will deploy the app using edge functions
-      // (https://vercel.com/docs/concepts/functions/edge-functions)
-      // rather than serverless functions
-      edge: true,
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-      // an array of dependencies that esbuild should treat
-      // as external when bundling functions
-      external: [],
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app
-      split: false
-    })
-  }
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
