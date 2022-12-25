aa40101c77547ab9a69897a8dbe5a490b9e88cf5
Najman <najman.husaini@utah.edu>
2022-12-25 21:33:51 +0800

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 
 export default {
-	kit: {
-		adapter: adapter({
-			out: 'mangle_website_production/build',
-			precompress: true
-		})
-	}
+  kit: {
+    // default options are shown
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: true,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    })
+  }
 };
