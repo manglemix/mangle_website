7fd939c9b74cae786060f74c857a82ee37e8e2fb
Najman <najman.husaini@utah.edu>
2023-01-03 21:25:13 +0800

    fixed error handling

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,13 +3,17 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, url }) {
-    let response = await fetch('https://manglemix.com/api/blogs?count=5');
-
-    if (!response.ok) {
-        throw error(500, "Could not load blogs");
+    try {
+        let response = await fetch('https://manglemix.com/api/blogs?count=5');
+        
+        return {
+            blogs: await response.json()
+        };
+    } catch (error) {
+        
     }
 
     return {
-        blogs: await response.json()
+        blogs: []
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
+	import { is_empty } from 'svelte/internal';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
 
-{#each data.blogs as post}
+{#if data.blogs.length == 0}
+	<br/>
+	<h3>Sorry, we could not load any blogs right now</h3>
+{:else}
+	{#each data.blogs as post}
 	{@html post.title}
 	{#if post.date != null}
 		<p><em>{post.date}</em></p>
 	{/if}
 	<p>{@html post.body}</p>
-{/each}
+	{/each}
+{/if}
