0d4c5b6b3be91caabb648b4535443e239a0ab7d2
Najman <najman.husaini@utah.edu>
2023-03-11 17:50:40 -0700

    simplified path

---
+++ src/routes/bola/+page.svelte
@@ -0,0 +1,5 @@
+<meta http-equiv="refresh" content="0; URL=https://gh.manglemix.com/bola">
+<link rel="canonical" href="https://gh.manglemix.com/bola">
+
+<h2>Hang Tight!</h2>
+<h2>You are being redirected</h2>
