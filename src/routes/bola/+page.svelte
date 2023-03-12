12b302064665e565344d0d78acd553a862df9f94
Najman <najman.husaini@utah.edu>
2023-03-11 21:17:42 -0700

    rewrite redirect

--- src/routes/bola/+page.svelte
+++ src/routes/bola/+page.svelte
@@ -1,4 +1,7 @@
-<meta http-equiv="refresh" content="0; URL=bola/index.html" />
-<link rel="canonical" href="bola/index.html" />
+<!-- <meta http-equiv="refresh" content="0; URL=bola/index.html" />
+<link rel="canonical" href="bola/index.html" /> -->
 
+<script>
+    window.location.replace("https://manglemix.com/bola/index.html");
+</script>
 <h3>Loading...</h3>
