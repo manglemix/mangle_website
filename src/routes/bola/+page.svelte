3546f687a854e727c6ed146e6fe6e84e5d51b45f
Najman <najman.husaini@utah.edu>
2023-03-11 21:32:09 -0700

    rewrite json again 2

--- src/routes/bola/+page.svelte
+++
@@ -1,7 +0,0 @@
-<!-- <meta http-equiv="refresh" content="0; URL=bola/index.html" />
-<link rel="canonical" href="bola/index.html" /> -->
-
-<script>
-    window.location.replace("https://manglemix.com/bola/index.html");
-</script>
-<h3>Loading...</h3>
